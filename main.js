8725d49d804114a8fba715a8f1bc3a2ce7c01d88
Philip Mocz <philip.mocz@gmail.com>
2022-11-19 19:45:58 -0800

    simplify

--- main.js
+++ main.js
@@ -4,21 +4,21 @@
     var helpButton = document.getElementById("help-button");
     var settingsButton = document.getElementById("settings-button");
     var helpScreen = document.getElementById("help-screen");
-    var settingsScreen = document.getElementById("settings-screen");
+    var settingsScreen = document.getElementById("settings-screen");  
     
-    var submitInput = document.getElementById("submit-input"); 
-    var submitButton = document.getElementById("submit-button");    
+    var s = [] 
+    s[0] = document.getElementById("s0");
+    s[1] = document.getElementById("s1");
+    s[2] = document.getElementById("s2");
+    s[3] = document.getElementById("s3");
+    s[4] = document.getElementById("s4");
+    s[5] = document.getElementById("s5");
+    s[6] = document.getElementById("s6");
+    s[7] = document.getElementById("s7");
+    s[8] = document.getElementById("s8");
+    s[9] = document.getElementById("s9");
     
-    var s0 = document.getElementById("s0");
-    var s1 = document.getElementById("s1");
-    var s2 = document.getElementById("s2");
-    var s3 = document.getElementById("s3");
-    var s4 = document.getElementById("s4");
-    var s5 = document.getElementById("s5");
-    var s6 = document.getElementById("s6");
-    var s7 = document.getElementById("s7");
-    var s8 = document.getElementById("s8");
-    var s9 = document.getElementById("s9");
+    var player = document.getElementById("player");
      
     var title = "Title"  
     var story = [];
@@ -40,21 +40,26 @@
 		showStory();	
 	}
 	
-	function showStory() {			
-		s0.className = 'storyline'; s0.innerHTML = story[0];
-		s1.className = 'storyline'; s1.innerHTML = story[1];
-		s2.className = 'storyline'; s2.innerHTML = story[2];
-		s3.className = 'storyline'; s3.innerHTML = story[3];
-		s4.className = 'storyline'; s4.innerHTML = story[4];
-		s5.className = 'storyline'; s5.innerHTML = story[5];
-		s6.className = 'storyline'; s6.innerHTML = story[6];
-		s7.className = 'storyline'; s7.innerHTML = story[7];
-		s8.className = 'storyline'; s8.innerHTML = story[8];
-		s9.className = 'storyline'; s9.innerHTML = story[9];
+	function showStory() {	
+		var i;
+		for (i = 0; i < 10; i++) {
+			s[i].className = 'storyline'; 
+			s[i].value = story[i];
+	    }
 	}
 	
 	function reveal() {
 		showStory();
+	    s[0].className = 'storylineactive1';
+	    s[1].className = 'storylineactive1';
+	    s[2].className = 'storylineactive2';
+	    s[3].className = 'storylineactive2';	
+	    s[4].className = 'storylineactive1';
+	    s[5].className = 'storylineactive1';
+	    s[6].className = 'storylineactive2';
+	    s[7].className = 'storylineactive2';
+	    s[8].className = 'storylineactive1';
+	    s[9].className = 'storylineactive1';
 		line++;
 		updateGameState();
 	}
@@ -68,67 +73,87 @@
 	/** Main Game Logic
 	 */
 	function updateGameState() {
+
+		for (i = 0; i < 10; i++) {
+			s[i].disabled=true;
+			s[i].placeholder="";
+		}
+
+		if (line < 10)	{
+			var i;
+			for (i = 0; i < 10; i++) s[i].value="";
+			s[line].disabled=false; 
+			s[line].focus();
+			s[line].placeholder="write the next line of the story ...";
+		}
 		
 		switch (line) {
 			case 0:
-				s0.className = 'storylineactive';
-				s1.className = 'storylinenext';
+				s[0].className = 'storylinenext1';
+				s[1].className = 'storylineactive1';
 				break;
 			case 1:
-				s0.className = 'storyline'; s0.innerHTML = story[0];
-				s1.className = 'storylineactive';
+			    s[0].value = story[0];
+				s[0].className = 'storylineactive1';
+				s[1].className = 'storylinenext1';
 				break;
 			case 2:
-				s0.className = 'storylinehidden'; s0.innerHTML = "";
-				s1.className = 'storyline'; s1.innerHTML = story[1];
-				s2.className = 'storylineactive';
-				s3.className = 'storylinenext';
+			    s[1].value = story[1];
+				s[0].className = 'storylinehidden';
+				s[1].className = 'storylineactive1';
+				s[2].className = 'storylinenext2';
+				s[3].className = 'storylineactive2';
 				break;
 			case 3:
-				s1.className = 'storylinehidden'; s1.innerHTML = "";
-				s2.className = 'storylinehidden'; s2.innerHTML = "";
-				s3.className = 'storylinenext';
+			    s[1].value = story[1];
+			    s[2].value = story[2];
+				s[2].className = 'storylineactive2';
+				s[3].className = 'storylinenext2';
 				break;
 			case 4:
-				s3.className = 'storyline'; s3.innerHTML = story[3];
-				s4.className = 'storylineactive';
-				s5.className = 'storylinenext';
+			    s[3].value = story[3];
+				s[1].className = 'storylinehidden';
+				s[2].className = 'storylinehidden';
+				s[3].className = 'storylineactive2';
+				s[4].className = 'storylinenext1';
+				s[5].className = 'storylineactive1';
 				break;
 			case 5:
-				s4.className = 'storyline'; s4.innerHTML = story[4];
-				s5.className = 'storylineactive';
+			    s[3].value = story[3];
+			    s[4].value = story[4];
+				s[4].className = 'storylineactive1';
+				s[5].className = 'storylinenext1';
 				break;
 			case 6:
-				sA3.className = 'storylinehidden'; s3.innerHTML = "";
-				s4.className = 'storylinehidden'; s4.innerHTML = "";
-				s5.className = 'storyline'; s5.innerHTML = story[5];
-				s6.className = 'storylinenext';
-				s7.className = 'storylinenext';
+			    s[5].value = story[5];
+				s[3].className = 'storylinehidden';
+				s[4].className = 'storylinehidden';
+				s[5].className = 'storylineactive1';
+				s[6].className = 'storylinenext2';
+				s[7].className = 'storylineactive2';
 				break;
 			case 7:
-				s5.className = 'storylinehidden'; s5.innerHTML = "";
-				s6.className = 'storylinehidden'; s6.innerHTML = "";
+			    s[5].value = story[5];
+			    s[6].value = story[6];
+				s[6].className = 'storylineactive2';
+				s[7].className = 'storylinenext2';
 				break;
 			case 8:
-				s7.className = 'storyline'; s7.innerHTML = story[7];
-				s8.className = 'storylineactive';
-				s9.className = 'storylinenext';
+			    s[7].value = story[7];
+			    s[5].className = 'storylinehidden';
+				s[6].className = 'storylinehidden';
+				s[7].className = 'storylineactive2';
+				s[8].className = 'storylinenext1';
+				s[9].className = 'storylineactive1';
 				break;
 			case 9:
-				s8.className = 'storyline'; s8.innerHTML = story[8];
-				s9.className = 'storylineactive';
+			    s[7].value = story[7];
+			    s[8].value = story[8];
+				s[8].className = 'storylineactive1';
+				s[9].className = 'storylinenext1';
 				break;
 			case 10:
-				s9.className = 'storyline'; s9.innerHTML = story[9];
-				//titleBox.className = 'visible';
-				//titleButton.className = 'visible';
-				break;
-			case 11:
-				s7.className = 'storylinehidden'; s7.innerHTML = "";
-				s8.className = 'storylinehidden'; s8.innerHTML = "";
-				s9.className = 'storylinehidden'; s9.innerHTML = "";
-				//titleBox.className = 'hidden';
-				//titleButton.className = 'hidden';
+				reveal();
 				break;
 		};
 			
@@ -146,36 +171,85 @@
     
 
     // Listen for enter in Input line
-    submitInput.addEventListener('keypress', function (e) {
-        var event = e || window.event;
-        var char = event.which || event.keyCode;
-        if (char == '13') submitButton.click();
-    });
-    // Submit Input line for story
-    submitButton.addEventListener('click', function () {
-        addLine(submitInput.value);
-        submitInput.value = "";   
-    });
-    
-    //// Listen for enter in title box A
-    //sendTitleBoxA.addEventListener('keypress', function (e) {
-    //    var event = e || window.event;
-    //    var char = event.which || event.keyCode;
-    //    if (char == '13')
-    //        titleButtonA.click();
-    //});
-    //// Send title
-    //titleButtonA.addEventListener('click', function () {
-    //    if (conn && conn.open) {
-    //        var msg = "0" + sendTitleBoxA.value;
-    //        sendTitleBoxA.value = "";
-    //        conn.send(msg);
-    //        console.log("Sent: " + msg);
-    //        addLine(msg);
-    //    } else {
-    //        console.log('Connection is closed');
-    //    }
-    //});
+	s[0].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[0].value);
+		}
+	});
+	
+	s[1].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[1].value);
+		}
+	});
+ 
+	s[2].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[2].value);
+		}
+	});
+
+	s[3].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[3].value);
+		}
+	});
+	    
+	s[4].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[4].value);
+		}
+	});
+	    
+	s[5].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[5].value);
+		}
+	});
+
+	s[6].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[6].value);
+		}
+	}); 
+ 
+	s[7].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[7].value);
+		}
+	});  
+
+	s[8].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[8].value);
+		}
+	}); 
+	
+	s[9].addEventListener('keypress', function (e) {
+		var event = e || window.event;
+		var char = event.which || event.keyCode;
+		if (char == '13') {
+			addLine(s[9].value);
+		}
+	}); 	
     
     // Reveal Stories
     //revealButton.addEventListener('click', reveal);
